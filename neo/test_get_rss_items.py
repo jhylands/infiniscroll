0a83aecdf8452d4f4a09f4bcfe937224171e54e2
jhylands <hylands.james@gmail.com>
2021-04-08 16:50:48 +0100

    First branch of neo4j using py2neo
     - Created some models which could be used to add the nodes of items

---
+++ neo/test_get_rss_items.py
@@ -0,0 +1,9 @@
+from get_rss_items import get_user, get_items
+
+def test_get_user():
+    assert get_user(1).name == "James"
+
+def test_get_items():
+    a = list(get_items(None))
+    print(a[0].url)
+    assert len(a)==2
