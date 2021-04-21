a9fff9377ad1cbcf0243b3ca8b59037d61681cfe
jhylands <hylands.james@gmail.com>
2021-04-21 22:40:06 +0100

    Moved message handling to new module
    - The purpose of the new module is to be able to add functionality to
    the user sending messages. The idea being it will be easier to add
    subscriptions and the like

--- server/message.py
+++ server/message.py
@@ -1,5 +1,6 @@
 # Class to represent the previous message objects
 
+
 class Message:
-    def __init__(self, message_text):
+    def __init__(self, message_text: str):
         self.text = message_text
--- server/server.py
+++ server/server.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, request, jsonify, make_response, send_from_directory, g
+from flask import Flask, request, jsonify, send_from_directory, g
 from flask_login import LoginManager, login_required, current_user
-from sqlalchemy import desc
 from auth import auth as auth_blueprint
 from db.user import User
-from db.message import Message, MessageStatus
 from db.db import Session
-
-import datetime
-
+from responder.main import NewMessageHandler, MessageLoader
 from item_manager import ItemManager
 
 app = Flask(__name__)
@@ -17,21 +13,21 @@ app = Flask(__name__)
 @login_required
 def index():
     """ Route to render the HTML """
-    return send_from_directory('../dist', "index.html")
+    return send_from_directory("../dist", "index.html")
 
 
 @app.route("/main.js")
 def js():
     """ Route to render the HTML """
-    return send_from_directory('../dist', "main.js")
+    return send_from_directory("../dist", "main.js")
 
 
-@app.route('/js/<path:path>')
+@app.route("/js/<path:path>")
 def send_js(path):
-    return send_from_directory('../dist', path)
+    return send_from_directory("../dist", path)
 
 
-@app.route("/load", methods=['POST'])
+@app.route("/load", methods=["POST"])
 @login_required
 def load():
     """ Route to return the posts """
@@ -40,95 +36,50 @@ def load():
     return jsonify(items)
 
 
-@app.route('/whoami/')
+@app.route("/whoami/")
 @login_required
 def whoami():
     return jsonify({"current user": current_user.id})
 
 
-@app.route('/previous_messages/', methods=['POST'])
+@app.route("/previous_messages/", methods=["POST"])
 @login_required
 def get_previous_messages_unknown_number():
     session = g.session
-    number_to_load = 10
-    messages = (
-        session
-        .query(Message)
-        .filter(Message.user_id == current_user.id)
-        .order_by(desc(Message.id))
-        .limit(number_to_load)
-        .all())
-    acc = [message.as_dict() for message in messages]
-    return jsonify(acc)
+    return jsonify(MessageLoader(session).user_id(current_user.id).load(10).send())
 
 
-@app.route('/previous_messages/<int:last_id>', methods=['POST'])
+@app.route("/previous_messages/<int:last_id>", methods=["POST"])
 @login_required
 def get_previous_messages(last_id):
     # init SQLAlchemy so we can use it later in our models
     session = g.session
-    number_to_load = 10
-    messages = (
-        session
-        .query(Message)
-        .filter(Message.user_id == current_user.id)
-        .filter(Message.id < last_id)
-        .order_by(desc(Message.id))
-        .limit(number_to_load)
-        .all())
-    acc = [message.as_dict() for message in messages]
-    return jsonify(acc)
+    return jsonify(
+        MessageLoader(session).user_id(current_user.id).last_id(last_id).send()
+    )
 
 
-class NewMessageHandler:
-    # in message
-    def __init__(self, message):
-        self.message = message
-
-    @staticmethod
-    def message_handler(message):
-        if message.startswith("/"):
-            return "search"
-
-    def do(self, session):
-        new_message = self.store_message(session, self.message)
-        response = self.message_handler(self.message)
-        if response:
-            new_response = self.store_message(session, response, MessageStatus.SERVER)
-            return [new_message.as_dict(), new_response.as_dict()]
-        else:
-            return [new_message.as_dict()]
-        
-
-    @staticmethod
-    def store_message(session: Session, message: str, status: MessageStatus =MessageStatus.USER)->Message:
-        now = datetime.datetime.now()
-        new_message = Message(
-            user_id=current_user.id,
-            message=message,
-            status=status,
-            timestamp=now)
-        session.add(new_message)
-        session.commit()
-        return new_message
-
-
-@app.route('/store_message', methods=['POST'])
+@app.route("/store_message", methods=["POST"])
 @login_required
 def store_message():
     # init SQLAlchemy so we can use it later in our models
     session = g.session
     message = request.json.get("message")
-    return jsonify(NewMessageHandler(message).do(session))
+    return jsonify(
+        NewMessageHandler(message).user_id(current_user.id).session(session).send()
+    )
+
 
 @app.before_request
 def attachdb():
     g.session = Session()
 
+
 @app.teardown_request
 def teardown_request(exception):
     g.session.close()
     Session.remove()
+
 
 @app.after_request
 def closedb(request):
@@ -136,18 +87,19 @@ def closedb(request):
     Session.remove()
     return request
 
+
 if __name__ == "__main__":
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config["SECRET_KEY"] = "9OLWxND4o83j4K4iuopO"
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
         session = g.session
         # since the user_id is just the primary key of our user table, use it in the query for the user
-        user =  session.query(User).filter(User.id==int(user_id)).one()
+        user = session.query(User).filter(User.id == int(user_id)).one()
         print(user.id)
         return user
 
