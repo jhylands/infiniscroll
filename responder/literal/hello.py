e5fff6d7401906a271766dc2ee03e2c91174c002
jhylands <hylands.james@gmail.com>
2021-04-26 22:29:13 +0100

    Simple literal of hello is working

--- responder/literal/hello.py
+++ responder/literal/hello.py
@@ -1,3 +1,4 @@
+from db.message import Message, MessageStatus
 from responder.message_handler_base import LiteralResponder
 from responder.message_handler_factory import MessageHandlerFactory
 
@@ -7,3 +8,12 @@ class Hello(LiteralResponder):
     @staticmethod
     def get_name():
         return "hello"
+
+    def run(self):
+        handler = self.handler
+        command = self.command
+        response = "Hi there!"
+        return [
+            handler.store_message(command, MessageStatus.USER).as_dict(),
+            handler.store_message(response, MessageStatus.SERVER).as_dict(),
+        ]
