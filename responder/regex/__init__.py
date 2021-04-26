16c7d2d6bf8b67967095b46e38a3bc934880bb77
jhylands <hylands.james@gmail.com>
2021-04-26 22:33:39 +0100

    Regex hello working

---
+++ responder/regex/__init__.py
@@ -0,0 +1,1 @@
+import responder.regex.hello
---
+++ responder/regex/hello.py
@@ -0,0 +1,19 @@
+from db.message import Message, MessageStatus
+from responder.message_handler_base import RegexResponder
+from responder.message_handler_factory import MessageHandlerFactory
+
+
+@MessageHandlerFactory.register()
+class Hello(RegexResponder):
+    @staticmethod
+    def get_name():
+        return "(?i)h(i|ello).*"
+
+    def run(self):
+        handler = self.handler
+        command = self.command
+        response = "Hi there!"
+        return [
+            handler.store_message(command, MessageStatus.USER).as_dict(),
+            handler.store_message(response, MessageStatus.SERVER).as_dict(),
+        ]
