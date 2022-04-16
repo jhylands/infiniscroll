95e6590608cb07598333f5d79a498fffd0983d78
jhylands <hylands.james@gmail.com>
2022-04-16 19:23:36 +0000

    Adding a run file
    I think there are some ofther elmeents that have beed added too.

---
+++ run.sh
@@ -0,0 +1,5 @@
+cd /root/documents/infiniscroll/
+export neocode=thebestpasswordicanthinkofthatcanalsogointheterminal
+export code=thebestpasswordintheworld
+export PYTHONPATH=.
+py3/bin/python server/server.py
